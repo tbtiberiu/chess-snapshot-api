742cbe49837e92453b60999d41a798ec9213776b
tiberiuioan35@gmail.com <tiberiuioan35@gmail.com>
2024-02-17 18:10:10 +0200

    Add jsonify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import cv2 as cv
 import numpy as np
 from detectors.chess_position_detector import ChessPositionDetector
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello_chess_snapshot():
     return 'Hello, Chess Snapshot!'
 
-@app.route('/get_chess_position', methods=['POST'])
+@app.route('/api/get_chess_position', methods=['POST'])
 def get_chess_position():
     if 'image' not in request.files:
         return 'No image uploaded', 400
@@ -23,7 +23,7 @@ def get_chess_position():
     chess_position_detector = ChessPositionDetector()
     fen = chess_position_detector.detect(original_image)
 
-    return { 'fen': fen }
+    return jsonify({'fen': fen})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
